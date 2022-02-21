5cc493c0eeec096294a66b75d449b8a298fbf4ae
Leon Kielstra <leon.kielstra@nedap.com>
2022-02-21 21:01:40 +0100

    Initial commit

---
+++ test/quicksort_c_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class QuicksortCTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::QuicksortC::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
